2c815fe48486d2235de978ed8e91d48fb39d4e66
Khen <alcantarakhen27@gmail.com>
2024-06-03 01:03:12 +0800

    Update

--- MS_Office_Installer.py
+++ MS_Office_Installer.py
@@ -7,13 +7,58 @@ height = 500
 selected = []
 
 
-class SelectApplication(customtkinter.CTk):
+class Main(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
         self.geometry(self.Center_Window_To_Display(width, height))
         self._set_appearance_mode("Light")
         self.title("MS Office")
+
+        self.mainFrame = customtkinter.CTkFrame(self, fg_color="white", border_width=1, border_color="orange",
+                                                corner_radius=0)
+        self.mainFrame.pack(padx=3, pady=3, fill="both", expand=True)
+        self.headerFrame = customtkinter.CTkFrame(self.mainFrame, fg_color="transparent", height=26, corner_radius=0)
+        self.headerFrame.pack(side="top", padx=1, pady=(1, 0), fill="both")
+        self.headerFrame.bind("<ButtonPress-1>", self.start_drag)
+        self.headerFrame.bind("<B1-Motion>", self.on_drag)
+        self.headerFrame.bind("<ButtonRelease-1>", self.stop_drag)
+        self.headerFrame.bind("<Double-Button-1>", self.minimize_window)
+
+        self.show_frame(SelectApplication)
+
+        self.footerFrame = customtkinter.CTkFrame(self.mainFrame, fg_color="transparent", height=26, corner_radius=0)
+        self.footerFrame.pack(side="top", padx=1, pady=(0, 1), fill="both")
+
+    def show_frame(self, page_class):
+        page_class(self.mainFrame).pack(side="top", padx=1, fill="both", expand=True)
+
+    def Center_Window_To_Display(self, _width: int, _height: int):
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        x = int((screen_width / 2) - (_width / 2))
+        y = int((screen_height / 2) - (_height / 2))
+        return f"{_width}x{_height}+{x}+{y}"
+
+    def start_drag(self, event):
+        self._drag_data = {'x': event.x, 'y': event.y}
+
+    def on_drag(self, event):
+        x = self.winfo_x() + event.x - self._drag_data['x']
+        y = self.winfo_y() + event.y - self._drag_data['y']
+        self.geometry(f"+{x}+{y}")
+
+    def stop_drag(self):
+        self._drag_data = None
+
+    def minimize_window(self):
+        self.withdraw()
+
+
+class SelectApplication(customtkinter.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.configure(fg_color="transparent")
 
         self.icons = {
             "Logo": Image.open("Image_Resources/logo.png"),
@@ -34,27 +79,14 @@ class SelectApplication(customtkinter.CTk):
         self.temp_file = "temp.xml"
 
         # Setup for each frame
-        self.mainFrame = customtkinter.CTkFrame(self, fg_color="white", border_width=1, border_color="orange",
-                                                corner_radius=0)
-        self.mainFrame.pack(padx=3, pady=3, fill="both", expand=True)
-        self.headerFrame = customtkinter.CTkFrame(self.mainFrame, fg_color="transparent", height=26, border_width=0,
-                                                  corner_radius=0)
-        self.headerFrame.pack(side="top", padx=1, pady=(1, 0), fill="both")
-        self.headerFrame.bind("<ButtonPress-1>", self.start_drag)
-        self.headerFrame.bind("<B1-Motion>", self.on_drag)
-        self.headerFrame.bind("<ButtonRelease-1>", self.stop_drag)
-        self.headerFrame.bind("<Double-Button-1>", self.minimize_window)
-        self.titleFrame = customtkinter.CTkFrame(self.mainFrame, fg_color="transparent", height=20, border_width=0,
+        self.titleFrame = customtkinter.CTkFrame(self, fg_color="transparent", height=20, border_width=0,
                                                  corner_radius=0)
         self.titleFrame.pack(side="top", padx=1, fill="both")
-        self.bodyFrame = customtkinter.CTkFrame(self.mainFrame, fg_color="transparent", border_width=0, corner_radius=0)
+        self.bodyFrame = customtkinter.CTkFrame(self, fg_color="transparent", border_width=0, corner_radius=0)
         self.bodyFrame.pack(side="top", padx=1, fill="both", expand=True)
-        self.body1Frame = customtkinter.CTkFrame(self.mainFrame, fg_color="transparent", border_width=0,
+        self.body1Frame = customtkinter.CTkFrame(self, fg_color="transparent", border_width=0,
                                                  corner_radius=0)
         self.body1Frame.pack(side="top", padx=1, fill="both")
-        self.footerFrame = customtkinter.CTkFrame(self.mainFrame, fg_color="transparent", height=26, border_width=0,
-                                                  corner_radius=0)
-        self.footerFrame.pack(side="top", padx=1, pady=(0, 1), fill="both")
 
         # Object inside title frame
         self.exitLabel = customtkinter.CTkLabel(self.titleFrame, text="", width=0, height=0,
@@ -69,31 +101,31 @@ class SelectApplication(customtkinter.CTk):
         self.appFrame = customtkinter.CTkFrame(self.bodyFrame, fg_color="transparent", height=26, border_width=0,
                                                corner_radius=0)
         self.appFrame.pack(side="top", pady=(50, 0))
-        self.AccessBtn = customtkinter.CTkButton(self.appFrame)
+        self.AccessBtn = customtkinter.CTkButton(self.appFrame, width=70, height=70)
         self.AccessBtn.pack(side="left", padx=2, pady=2)
-        self.SkypeBtn = customtkinter.CTkButton(self.appFrame)
+        self.SkypeBtn = customtkinter.CTkButton(self.appFrame, width=70, height=70)
         self.SkypeBtn.pack(side="left", padx=2, pady=2)
-        self.OneNoteBtn = customtkinter.CTkButton(self.appFrame)
+        self.OneNoteBtn = customtkinter.CTkButton(self.appFrame, width=70, height=70)
         self.OneNoteBtn.pack(side="left", padx=2, pady=2)
 
         self.appFrame1 = customtkinter.CTkFrame(self.bodyFrame, fg_color="transparent", height=26, border_width=0,
                                                 corner_radius=70)
         self.appFrame1.pack(side="top")
-        self.PowerPointBtn = customtkinter.CTkButton(self.appFrame1)
+        self.PowerPointBtn = customtkinter.CTkButton(self.appFrame1, width=70, height=70)
         self.PowerPointBtn.pack(side="left", padx=2, pady=2)
-        self.WordBtn = customtkinter.CTkButton(self.appFrame1)
+        self.WordBtn = customtkinter.CTkButton(self.appFrame1, width=70, height=70)
         self.WordBtn.pack(side="left", padx=2, pady=2)
-        self.ExcelBtn = customtkinter.CTkButton(self.appFrame1)
+        self.ExcelBtn = customtkinter.CTkButton(self.appFrame1, width=70, height=70)
         self.ExcelBtn.pack(side="left", padx=2, pady=2)
-        self.OneDriveBtn = customtkinter.CTkButton(self.appFrame1)
+        self.OneDriveBtn = customtkinter.CTkButton(self.appFrame1, width=70, height=70)
         self.OneDriveBtn.pack(side="left", padx=2, pady=2)
 
         self.appFrame2 = customtkinter.CTkFrame(self.bodyFrame, fg_color="transparent", height=26, border_width=0,
                                                 corner_radius=0)
         self.appFrame2.pack(side="top")
-        self.OutlookBtn = customtkinter.CTkButton(self.appFrame2)
+        self.OutlookBtn = customtkinter.CTkButton(self.appFrame2, width=70, height=70)
         self.OutlookBtn.pack(side="left", padx=2, pady=2)
-        self.PublisherBtn = customtkinter.CTkButton(self.appFrame2)
+        self.PublisherBtn = customtkinter.CTkButton(self.appFrame2, width=70, height=70)
         self.PublisherBtn.pack(side="left", padx=2, pady=2)
 
         self.AccessBtn.bindtags(["Access"])
@@ -120,7 +152,7 @@ class SelectApplication(customtkinter.CTk):
 
         for button in self.apps_button:
             button.configure(text="", image=self.icon(button.bindtags()[0], 0.05), fg_color="transparent",
-                             corner_radius=0, width=70, height=70, hover=False)
+                             corner_radius=0, hover=False)
             button.bind("<Button-1>", lambda event, btn=button: self.Click(btn))
             button.bind("<Enter>", lambda event, btn=button: self.Enter(btn))
             button.bind("<Leave>", lambda event, btn=button: self.Leave(btn))
@@ -132,15 +164,6 @@ class SelectApplication(customtkinter.CTk):
                                                   border_color="gray", state="disabled", command=self.Install,
                                                   font=customtkinter.CTkFont(family="Segoe UI", size=14, weight="bold"))
         self.installBtn.pack(pady=26)
-
-        self.text =  customtkinter.CTk
-
-    def Center_Window_To_Display(self, _width: int, _height: int):
-        screen_width = self.winfo_screenwidth()
-        screen_height = self.winfo_screenheight()
-        x = int((screen_width / 2) - (_width / 2))
-        y = int((screen_height / 2) - (_height / 2))
-        return f"{_width}x{_height}+{x}+{y}"
 
     def icon(self, icon_name: str, scale_factor: float):
         global width, height
@@ -182,24 +205,9 @@ class SelectApplication(customtkinter.CTk):
                 for exclude_app in product.findall(".//ExcludeApp"):
                     if exclude_app.attrib.get("ID") == id_list:
                         product.remove(exclude_app)
-                        print(exclude_app)
 
         tree.write(self.temp_file)
 
-    def start_drag(self, event):
-        self._drag_data = {'x': event.x, 'y': event.y}
-
-    def on_drag(self, event):
-        x = self.winfo_x() + event.x - self._drag_data['x']
-        y = self.winfo_y() + event.y - self._drag_data['y']
-        self.geometry(f"+{x}+{y}")
-
-    def stop_drag(self):
-        self._drag_data = None
-
-    def minimize_window(self):
-        self.withdraw()
-
 
 if __name__ == "__main__":
-    SelectApplication().mainloop()
+    Main().mainloop()
